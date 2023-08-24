ed877df329d45b72ac0f79441514f912c29a3694
Bit0r <nie_wang@outlook.com>
2023-08-24 15:34:04 +0800

    📦 引入snippets和black配置

--- global/configs.fish
+++ global/configs.fish
@@ -1,6 +1,13 @@
 #!/usr/bin/fish
 
+# 复制snippets
+mkdir -p ~/.config/Code/User/snippets
+cp snippets/*.json ~/.config/Code/User/snippets/
+
+# 复制settings和keybindings
 cp *.json ~/.config/Code/User/
+
+# 安装插件
 for ext in (cat extensions.txt)
 	code --install-extension $ext
 end
