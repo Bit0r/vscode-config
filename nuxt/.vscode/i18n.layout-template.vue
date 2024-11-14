157c3563d683bcb27701729a5ab736e006190e43
Bit0r <nie_wang@outlook.com>
2024-11-14 23:16:25 +0800

    feat: 添加新的 VS Code 扩展、更新全局设置和键绑定

---
+++ nuxt/.vscode/i18n.layout-template.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<slot />
+	</div>
+</template>
+
+<i18n lang="yaml">
+en:
+
+zh-Hans:
+
+ar:
+</i18n>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt/.vscode/i18n.page-template.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<i18n lang="yaml">
+en:
+
+zh-Hans:
+
+ar:
+</i18n>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
